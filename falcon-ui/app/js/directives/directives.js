38d5b04da235b9f8eb01b0348ed0de8c7f8dfd72
Armando Reyna <armando.reyna@globant.com>
2015-01-07 09:40:54 -0600

    fix date picker format

--- falcon-ui/app/js/directives/directives.js
+++ falcon-ui/app/js/directives/directives.js
@@ -87,5 +87,24 @@
       templateUrl: 'html/directives/timeZoneSelectDv.html'
     };
   });
+  
+  directivesModule.directive('simpleDate', ['$filter', function($filter) {
+  	return {
+	    require: 'ngModel',
+	    link: function(scope, element, attrs, ngModelController) {
+	    	ngModelController.$parsers.push(function(data) {
+	        //convert data from view format to model format
+	        return data;
+	      });
+	      ngModelController.$formatters.push(function(date) {
+	        //convert data from model format to view format
+	      	if(date !== ""){
+	      		date = $filter('date')(date, 'MM/dd/yyyy');
+	      	}
+	        return date;
+	      });
+	    }
+	  };
+	}]);
 
 }());
