979480626f8967fd901ed772a572aab6c9db20fd
santiagoRebella <santiago_rebella@hotmail.com>
2015-02-04 20:00:21 -0200

    clean up of form bk

--- mirroring-ui/src/app/validation/validation.js
+++ mirroring-ui/src/app/validation/validation.js
@@ -92,20 +92,28 @@
       },
       checkPatterns = {
         name: new RegExp("^[a-zA-Z0-9]{1,39}$"),
-        id: new RegExp("^(([a-zA-Z]([\\-a-zA-Z0-9])*){1,39})$"),
-        password: new RegExp("^(([a-zA-Z]([\\-a-zA-Z0-9])*){1,39})$"),
-        freeText: new RegExp("^([\\sa-zA-Z0-9]){1,40}$"),
         alpha: new RegExp("^([a-zA-Z0-9]){1,100}$"),
-        commaSeparated: new RegExp("^[a-zA-Z0-9,]{1,80}$"),
-        unixId: new RegExp("^([a-z_][a-z0-9-_\\.\\-]{0,30})$"),
-        unixPermissions: new RegExp("^((([0-7]){1,4})|(\\*))$"),
         osPath: new RegExp("^[^\\0 ]+$"),
         url: new RegExp("^[^\\0 ]+\\.[a-zA-Z0-9]{1,3}$"),
-        twoDigits: new RegExp("^([0-9]){1,2}$"),
         number: new RegExp("^([0-9]){1,40}$"),
+        twoDigits: new RegExp("^([0-9]){1,2}$"),
+        email: new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$"),
+        unixPermissions: new RegExp("^((([0-7]){1,4})|(\\*))$"),
+        id: new RegExp("^(([a-zA-Z]([\\-a-zA-Z0-9])*){1,39})$"),
+        freeText: new RegExp("^([\\sa-zA-Z0-9]){1,40}$"),
+
+        password: new RegExp("^(([a-zA-Z]([\\-a-zA-Z0-9])*){1,39})$"),
+        
+
+        commaSeparated: new RegExp("^[a-zA-Z0-9,]{1,80}$"),
+        unixId: new RegExp("^([a-z_][a-z0-9-_\\.\\-]{0,30})$"),
+
+
+
+
+
         tableUri: new RegExp("^[^\\0]+$"),
         versionNumbers: new RegExp("^[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{1,2}$"),
-        email: new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")
       };
 
     function acceptOnlyNumber(evt) {
@@ -155,9 +163,7 @@
       messages: checkMessages,
       patterns: checkPatterns,
       show: false,
-
       nameAvailable: true,
-      /*displayValidations: {show: false, nameShow: false},*/
       acceptOnlyNumber: acceptOnlyNumber,
       acceptNoSpaces: acceptNoSpaces
     };
