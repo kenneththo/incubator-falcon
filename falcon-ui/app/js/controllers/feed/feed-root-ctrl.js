4f8826d495cd4b90d242c0b1a73d2d188a4cfa68
Vishal Kadam <vishal.4kadam@gmail.com>
2015-05-23 00:05:34 -0400

    Vishal: Partial feed screen

--- falcon-ui/app/js/controllers/feed/feed-root-ctrl.js
+++ falcon-ui/app/js/controllers/feed/feed-root-ctrl.js
@@ -116,6 +116,10 @@
           return route === $state.$current.name;
         };
 
+        $scope.isCompleted = function (route) {
+          return $state.get(route).data && $state.get(route).data.completed;
+        };
+
         $scope.parseDate = function(input) {
           return input ? input.split('T')[0] : input;
         };
@@ -174,6 +178,8 @@
           }
         });
         $scope.goNext = function (formInvalid, stateName) {
+          $state.current.data = $state.current.data || {};
+          $state.current.data.completed = !formInvalid;
 
           SpinnersFlag.show = true;
 
@@ -185,7 +191,6 @@
           }
           validationService.displayValidations.show = false;
           validationService.displayValidations.nameShow = false;
-
           $state.go(stateName);
 
 
